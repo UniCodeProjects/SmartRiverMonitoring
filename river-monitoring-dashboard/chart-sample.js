49ec5ae81e60957533b8e6d476da7ab4b66867bf
Francesco Cipollone <francesco.cipollone@studio.unibo.it>
2024-03-22 01:05:51 +0100

    Added base html page and base graph

--- river-monitoring-dashboard/chart-sample.js
+++ river-monitoring-dashboard/chart-sample.js
@@ -1,19 +1,29 @@
 const ctx = document.getElementById('myChart');
   
     new Chart(ctx, {
-      type: 'bar',
+      type: 'line',
       data: {
-        labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
+        labels: [0, 10, 20, 30, 40, 50, 60],
         datasets: [{
-          label: '# of Votes',
-          data: [12, 19, 3, 5, 2, 3],
+          label: 'Water level',
+          data: [50, 30, 60, 5, 70, 75, 80],
           borderWidth: 1
         }]
       },
       options: {
         scales: {
+          x: {
+            type: 'linear',
+            title: {
+              display: true,
+              text: 'Time (seconds)'
+            }
+          },
           y: {
-            beginAtZero: true
+            title: {
+              display: true,
+              text: 'Water Level (meters)'
+            }
           }
         }
       }
