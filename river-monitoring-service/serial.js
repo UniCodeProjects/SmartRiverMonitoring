46628d7b9cf42d53fd5d1202174ad0e3e9e0dc47
Sofia Cipollone <sofia.cipollone5@gmail.com>
2024-03-29 18:25:24 +0100

    Added arduino serial port auto detect

---
+++ river-monitoring-service/serial.js
@@ -0,0 +1,10 @@
+const { autoDetect } = require('@serialport/bindings-cpp');
+
+async function getArduinoPort() {
+    const list = await autoDetect().list();
+    const filtered = list.filter(port => port.manufacturer.toLowerCase().includes('arduino'))
+    if (filtered.length === 0) {
+        throw new Error("Arduino port not found");
+    }
+    return filtered;
+}
