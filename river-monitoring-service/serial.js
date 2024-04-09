ae260b22f9b53765387685d3ef5834da2785342a
Sofia Cipollone <sofia.cipollone5@gmail.com>
2024-04-09 19:56:37 +0200

    Added api to write and read on the serial line

--- river-monitoring-service/serial.js
+++ river-monitoring-service/serial.js
@@ -1,4 +1,5 @@
 const { autoDetect } = require('@serialport/bindings-cpp');
+const { SerialPort } = require('serialport');
 
 async function getArduinoPort() {
     const list = await autoDetect().list();
@@ -9,4 +10,30 @@ async function getArduinoPort() {
     return filtered[0].path;
 }
 
-module.exports = { getArduinoPort }
+function serialWrite(port, content) {
+    port.then(actualPort => {
+        if (!actualPort instanceof SerialPort) {
+            throw new Error("Not a SerialPort object.");
+        }
+        if (!content.endsWith('\n')) {
+            content += '\n';
+        }
+        actualPort.write(content);
+    });
+}
+
+function serialRead(port, callback) {
+    let buffer = '';
+    port.then(actualPort => {
+        const onDataReceived = data => {
+            buffer += data.toString();
+            if (buffer.endsWith('\n')) {
+                callback(buffer);
+                actualPort.off('data', onDataReceived);
+            }
+        }
+        actualPort.on('data', onDataReceived);
+    })
+}
+
+module.exports = { getArduinoPort, serialWrite, serialRead }
