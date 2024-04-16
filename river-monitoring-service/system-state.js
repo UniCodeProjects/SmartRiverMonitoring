84fec023e0360a6dba06f121e6a468ae002e4f1c
Francesco Cipollone <francesco.cipollone@studio.unibo.it>
2024-04-16 19:19:19 +0200

    Added system state enum

---
+++ river-monitoring-service/system-state.js
@@ -0,0 +1,9 @@
+const SystemState = {
+    Normal: 'NORMAL',
+    AlarmTooLow: 'ALARM-TOO-LOW',
+    PreAlarmTooHigh: "PRE-ALARM-TOO-HIGH",
+    AlarmTooHigh: 'ALARM-TOO-HIGH',
+    AlarmTooHighCritic: 'ALARM-TOO-HIGH-CRITIC'
+}
+
+module.exports = SystemState
