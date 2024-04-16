ab589c5c655a01c81a048db3da6b6e857f4131a3
Francesco Cipollone <francesco.cipollone@studio.unibo.it>
2024-04-16 20:08:06 +0200

    Added sample period property in SystemState

--- river-monitoring-service/system-state.js
+++ river-monitoring-service/system-state.js
@@ -1,9 +1,29 @@
+const Period = {
+    P1: 1000,
+    P2: 500
+}
+
 const SystemState = {
-    Normal: 'NORMAL',
-    AlarmTooLow: 'ALARM-TOO-LOW',
-    PreAlarmTooHigh: "PRE-ALARM-TOO-HIGH",
-    AlarmTooHigh: 'ALARM-TOO-HIGH',
-    AlarmTooHighCritic: 'ALARM-TOO-HIGH-CRITIC'
+    Normal: {
+        name: "NORMAL",
+        samplePeriod: Period.P1
+    },
+    AlarmTooLow: {
+        name: "ALARM-TOO-LOW",
+        samplePeriod: Period.P1
+    },
+    PreAlarmTooHigh: {
+        name: "PRE-ALARM-TOO-HIGH",
+        samplePeriod: Period.P2
+    },
+    AlarmTooHigh: {
+        name: "ALARM-TOO-HIGH",
+        samplePeriod: Period.P2
+    },
+    AlarmTooHighCritic: {
+        name: "ALARM-TOO-HIGH-CRITIC",
+        samplePeriod: Period.P2
+    }
 }
 
 module.exports = SystemState
