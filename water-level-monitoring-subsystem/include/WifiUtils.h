7af90d564757f354820d6df70307e2cd03b8239e
Raffaele Francesco Marrazzo <raffaele.marrazzo@studio.unibo.it>
2024-04-17 17:03:07 +0200

    Added mqtt and WiFi modules documentation

--- water-level-monitoring-subsystem/include/WifiUtils.h
+++ water-level-monitoring-subsystem/include/WifiUtils.h
@@ -1,7 +1,13 @@
 #ifndef WIFI_UTILS_H
 #define WIFI_UTILS_H
 
-void setupWifi(const char* ssid, const char* password);
+/// @brief Connects to the WiFi with the provided ssid and password, if exists.
+/// @param ssid the WiFi ssid.
+/// @param password the WiFi password.
+void connectToWiFi(const char* ssid, const char* password);
+
+/// @brief Returns whether the device is connected to WiFi.
+/// @return `true` if the device is connected to WiFi, `false` otherwise.
 bool isConnectedToWiFi();
 
 #endif
