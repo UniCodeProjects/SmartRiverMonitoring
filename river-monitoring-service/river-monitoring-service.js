e57f316653b8428cfe1f3b5b3569c46866be3ff9
Sofia Cipollone <sofia.cipollone5@gmail.com>
2024-04-17 17:46:19 +0200

    Added logic for publishing the correct sampling period via mqtt

---
+++ river-monitoring-service/river-monitoring-service.js
@@ -0,0 +1,37 @@
+const mqtt = require("mqtt");
+const WaterLevel = require("./water-level");
+const SystemState = require("./system-state");
+
+const client = mqtt.connect("mqtt://broker.mqtt-dashboard.com");
+const waterLevelTopic = "water-level";
+
+client.on("connect", () => {
+    client.subscribe(waterLevelTopic, err => {
+        if (err) {
+            throw err;
+        }
+    });
+});
+
+client.on("message", (topic, message) => {
+    if (topic != waterLevelTopic) {
+        throw new Error(`Unknown topic: ${topic}`);
+    }
+    const currentWaterLevel = Number(message);
+    if (currentWaterLevel === NaN) {
+        throw new Error(`Received water level was NaN: ${message}`);
+    }
+    if (currentWaterLevel >= WaterLevel.WL1 && currentWaterLevel <= WaterLevel.WL2) {
+        client.publish(waterLevelTopic, SystemState.Normal.samplePeriod);
+    } else if (currentWaterLevel < WaterLevel.WL1) {
+        client.publish(waterLevelTopic, SystemState.AlarmTooLow.samplePeriod);
+    } else if (currentWaterLevel > WaterLevel.WL2) {
+        if (currentWaterLevel <= WaterLevel.WL3) {
+            client.publish(waterLevelTopic, SystemState.PreAlarmTooHigh.samplePeriod);
+        } else if (currentWaterLevel > WaterLevel.WL3 && currentWaterLevel <= WaterLevel.WL4) {
+            client.publish(waterLevelTopic, SystemState.AlarmTooHigh.samplePeriod);
+        } else if (currentWaterLevel > WaterLevel.WL4) {
+            client.publish(waterLevelTopic, SystemState.AlarmTooHighCritic.samplePeriod);
+        }
+    }
+});
