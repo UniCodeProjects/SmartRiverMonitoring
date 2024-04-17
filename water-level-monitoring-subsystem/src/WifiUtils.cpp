ba2a5cbeffc73c4bc663e489b6c37ed2fabaaac6
Raffaele Francesco Marrazzo <raffaele.marrazzo@studio.unibo.it>
2024-04-17 16:19:06 +0200

    Removed unused method in WifiUtils. Replaced timeout macro with the maximum number of attempts in WifiUtils and MqttUtils modules

--- water-level-monitoring-subsystem/src/WifiUtils.cpp
+++ water-level-monitoring-subsystem/src/WifiUtils.cpp
@@ -1,41 +1,24 @@
 #include "../include/WifiUtils.h"
 #include <WiFi.h>
 
-#define WIFI_CONNECTION_TIMEOUT_MILLIS 5000
+#define MAX_ATTEMPTS 10
 #define WIFI_CONNECTION_DELTA_MILLIS 500
 
+static bool isModeSet = false;
+
 void setupWifi(const char* ssid, const char* password) {
-    // wl_status_t status;
-    // WiFi.mode(WIFI_STA);
-    // while (true) {
-    //     Serial.println(status);
-    //     if (status == WL_IDLE_STATUS) {
-    //         // Serial.println("Connecting");
-    //         continue;
-    //     }
-    //     if (status == WL_CONNECTED) {
-    //         // Serial.println("Connected");
-    //         break;
-    //     }
-    //     // Serial.println("Not connected, not connecting, beginning a wifi connection");
-    //     status = WiFi.begin(ssid, password);
-    // }
-    int timeElapsedMillis = 0;
-    WiFi.mode(WIFI_STA);
-    while (WiFi.status() != WL_CONNECTED) {
-        if (timeElapsedMillis >= WIFI_CONNECTION_TIMEOUT_MILLIS) {
-            return;
-        }
+    int attempts = 0;
+    if (!isModeSet) {
+        WiFi.mode(WIFI_STA);
+        isModeSet = true;
+    }
+    while (!isConnectedToWiFi() && attempts < MAX_ATTEMPTS) {
         WiFi.begin(ssid, password);
         delay(WIFI_CONNECTION_DELTA_MILLIS);
-        timeElapsedMillis += WIFI_CONNECTION_DELTA_MILLIS;
+        attempts++;
     }
 }
 
 bool isConnectedToWiFi() {
     return WiFi.status() == WL_CONNECTED;
-}
-
-bool isConnectingToWiFi() {
-    return WiFi.status() == WL_IDLE_STATUS;
 }
