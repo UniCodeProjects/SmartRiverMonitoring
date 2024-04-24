66daa912cf0177d547642ae21c723790bd3f552a
Francesco Cipollone <francesco.cipollone@studio.unibo.it>
2024-04-24 17:57:26 +0200

    Fixed type error bug in server

--- river-monitoring-service/river-monitoring-service.js
+++ river-monitoring-service/river-monitoring-service.js
@@ -12,28 +12,28 @@ client.on("connect", () => {
             throw err;
         }
     });
-    client.publish(samplePeriodTopic, SystemState.Normal.samplePeriod + '\n', { retain: true });
+    client.publish(samplePeriodTopic, `${SystemState.Normal.samplePeriod}\n`, { retain: true });
 });
 
 client.on("message", (topic, message) => {
-    if (topic != waterLevelTopic) {
-        throw new Error(`Unknown topic: ${topic}`);
+    if (topic.toString() != waterLevelTopic) {
+        throw new Error(`Unknown topic: ${topic.toString()}`);
     }
-    const currentWaterLevel = Number(message);
-    if (currentWaterLevel === NaN) {
-        throw new Error(`Received water level was NaN: ${message}`);
+    const currentWaterLevel = parseFloat(message.toString());
+    if (isNaN(currentWaterLevel)) {
+        throw new Error(`Received water level was NaN: ${message.toString()}`);
     }
     if (currentWaterLevel >= WaterLevel.WL1 && currentWaterLevel <= WaterLevel.WL2) {
-        client.publish(samplePeriodTopic, SystemState.Normal.samplePeriod);
+        client.publish(samplePeriodTopic, `${SystemState.Normal.samplePeriod}\n`);
     } else if (currentWaterLevel < WaterLevel.WL1) {
-        client.publish(samplePeriodTopic, SystemState.AlarmTooLow.samplePeriod);
+        client.publish(samplePeriodTopic, `${SystemState.AlarmTooLow.samplePeriod}\n`);
     } else if (currentWaterLevel > WaterLevel.WL2) {
         if (currentWaterLevel <= WaterLevel.WL3) {
-            client.publish(samplePeriodTopic, SystemState.PreAlarmTooHigh.samplePeriod);
+            client.publish(samplePeriodTopic, `${SystemState.PreAlarmTooHigh.samplePeriod}\n`);
         } else if (currentWaterLevel > WaterLevel.WL3 && currentWaterLevel <= WaterLevel.WL4) {
-            client.publish(samplePeriodTopic, SystemState.AlarmTooHigh.samplePeriod);
+            client.publish(samplePeriodTopic, `${SystemState.AlarmTooHigh.samplePeriod}\n`);
         } else if (currentWaterLevel > WaterLevel.WL4) {
-            client.publish(samplePeriodTopic, SystemState.AlarmTooHighCritic.samplePeriod);
+            client.publish(samplePeriodTopic, `${SystemState.AlarmTooHighCritic.samplePeriod}\n`);
         }
     }
 });
