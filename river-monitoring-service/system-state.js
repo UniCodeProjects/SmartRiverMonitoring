9dfd838fc86becbcfb7142ab13e4a0f63dd46b4f
Francesco Cipollone <francesco.cipollone@studio.unibo.it>
2024-04-30 18:42:11 +0200

    Updated SystemState to include frontend status badge styling

--- river-monitoring-service/system-state.js
+++ river-monitoring-service/system-state.js
@@ -6,23 +6,48 @@ const Period = {
 const SystemState = {
     Normal: {
         name: "NORMAL",
-        samplePeriod: Period.P1
+        samplePeriod: Period.P1,
+        Badge: {
+            name: "Normal",
+            icon: `<i class="bi bi-check-lg fw-bold pe-1" style="-webkit-text-stroke: 0.5px;"></i>`,
+            style: "border-success text-success"
+        }
     },
     AlarmTooLow: {
         name: "ALARM-TOO-LOW",
-        samplePeriod: Period.P1
+        samplePeriod: Period.P1,
+        Badge: {
+            name: "Low",
+            icon: `<i class="bi bi-chevron-down fw-bold pe-1" style="-webkit-text-stroke: 0.5px;"></i>`,
+            style: "border-warning text-warning"
+        }
     },
     PreAlarmTooHigh: {
         name: "PRE-ALARM-TOO-HIGH",
-        samplePeriod: Period.P2
+        samplePeriod: Period.P2,
+        Badge: {
+            name: "High",
+            icon: `<i class="bi bi-dash-lg fw-bold pe-1" style="-webkit-text-stroke: 0.5px;"></i>`,
+            style: "border-warning text-warning"
+        }
     },
     AlarmTooHigh: {
         name: "ALARM-TOO-HIGH",
-        samplePeriod: Period.P2
+        samplePeriod: Period.P2,
+        Badge: {
+            name: "High",
+            icon: `<i class="bi bi-chevron-up fw-bold pe-1" style="-webkit-text-stroke: 0.5px;"></i>`,
+            style: "border-warning text-warning"
+        }
     },
     AlarmTooHighCritic: {
         name: "ALARM-TOO-HIGH-CRITIC",
-        samplePeriod: Period.P2
+        samplePeriod: Period.P2,
+        Badge: {
+            name: "High",
+            icon: `<i class="bi bi-chevron-double-up fw-bold pe-1" style="-webkit-text-stroke: 0.5px;"></i>`,
+            style: "border-danger text-danger"
+        }
     }
 }
 
