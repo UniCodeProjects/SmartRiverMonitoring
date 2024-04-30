98498dbb303694b9d4032a97052484a8cf426aa1
Raffaele Francesco Marrazzo <raffaele.marrazzo@studio.unibo.it>
2024-04-30 16:39:54 +0200

    Basic implementation of automatic task in Arduino

---
+++ water-channel-controller/include/task/AutomaticTask.h
@@ -0,0 +1,22 @@
+#ifndef AUTOMATIC_TASK_H
+#define AUTOMATIC_TASK_H
+
+#include <LiquidCrystal_I2C.h>
+
+#include "../include/components/Valve.h"
+#include "../include/components/ValveKnob.h"
+#include "TaskImpl.h"
+
+class AutomaticTask : public TaskImpl {
+   public:
+    AutomaticTask(LiquidCrystal_I2C* const monitor, Valve* const valve, const int period);
+    void start();
+
+   private:
+    LiquidCrystal_I2C* monitor;
+    Valve* valve;
+
+    void printAndSetValveLevel(const int level);
+};
+
+#endif
