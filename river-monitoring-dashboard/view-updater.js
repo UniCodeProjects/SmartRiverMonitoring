6b582c7bb72b122cfa586573197480501e209d3f
Francesco Cipollone <francesco.cipollone@studio.unibo.it>
2024-05-01 17:39:45 +0200

    Code cleanup in view-updater

--- river-monitoring-dashboard/view-updater.js
+++ river-monitoring-dashboard/view-updater.js
@@ -1,11 +1,3 @@
-const SystemState = {
-    Normal: 'NORMAL',
-    AlarmTooLow: 'ALARM-TOO-LOW',
-    PreAlarmTooHigh: "PRE-ALARM-TOO-HIGH",
-    AlarmTooHigh: 'ALARM-TOO-HIGH',
-    AlarmTooHighCritic: 'ALARM-TOO-HIGH-CRITIC'
-}
-
 const ControlMode = {
     Automatic: 'Automatic',
     Manual: 'Manual'
@@ -40,49 +32,12 @@ function toggleManualMode() {
     }
 }
 
-function setRangeValue(value) {
-    document.getElementById('valve-range').value = value;
-}
-
 function updateProgressBar(value) {
     const progressBar = document.getElementById('valve-bar');
     const valuePercentage = value + '%';
     progressBar.style.width = valuePercentage;
     progressBar.innerHTML = valuePercentage;
     progressBar.setAttribute('aria-valuenow', value);
-}
-
-function updateStatusBadge(state) {
-    const statusBadge = document.getElementById('status-badge');
-    const borderColourItem = statusBadge.classList.item(statusBadge.classList.length - 2);
-    const textColourItem = statusBadge.classList.item(statusBadge.classList.length - 1);
-    statusBadge.classList.remove(borderColourItem, textColourItem);
-    const statusText = document.getElementById('status-text');
-    switch (state) {
-        case SystemState.Normal:
-            statusText.innerHTML = `<i class="bi bi-check-lg fw-bold pe-1" style="-webkit-text-stroke: 0.5px;"></i>Normal`;
-            statusBadge.classList.add('border-success', 'text-success');
-            break;
-        case SystemState.AlarmTooLow:
-            statusText.innerHTML = `<i class="bi bi-chevron-down fw-bold pe-1" style="-webkit-text-stroke: 0.5px;"></i>Low`;
-            statusBadge.classList.add('border-warning', 'text-warning');
-            break;
-        case SystemState.PreAlarmTooHigh:
-            statusText.innerHTML = `<i class="bi bi-dash-lg fw-bold pe-1" style="-webkit-text-stroke: 0.5px;"></i>High`;
-            statusBadge.classList.add('border-warning', 'text-warning');
-            break;
-        case SystemState.AlarmTooHigh:
-            statusText.innerHTML = `<i class="bi bi-chevron-up fw-bold pe-1" style="-webkit-text-stroke: 0.5px;"></i>High`;
-            statusBadge.classList.add('border-warning', 'text-warning');
-            break;
-        case SystemState.AlarmTooHighCritic:
-            statusText.innerHTML = `<i class="bi bi-chevron-double-up fw-bold pe-1" style="-webkit-text-stroke: 0.5px;"></i>High`;
-            statusBadge.classList.add('border-danger', 'text-danger');
-            break;
-        default:
-            console.error("Unknown state: ", state);
-            break;
-    }
 }
 
 function toggleValveOpeningInput(show = true) {
