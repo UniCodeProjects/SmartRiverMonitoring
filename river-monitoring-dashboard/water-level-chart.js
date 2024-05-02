db28378ae385d76f1e7428c49d2c380da0c9b7a7
Francesco Cipollone <francesco.cipollone@studio.unibo.it>
2024-05-02 18:08:13 +0200

    Refactored chart code to use 4 spaces for indentation

--- river-monitoring-dashboard/water-level-chart.js
+++ river-monitoring-dashboard/water-level-chart.js
@@ -1,47 +1,47 @@
 const bootstrapStyle = getComputedStyle(document.body);
 
 const chart = new Chart(document.getElementById('waterLevelChart'), {
-  type: 'line',
-  data: {
-    datasets: [{
-      label: 'Water level',
-      borderWidth: 1,
-      tension: 0.3,
-      backgroundColor: bootstrapStyle.getPropertyValue('--bs-primary-emphasis'),
-      borderColor: bootstrapStyle.getPropertyValue('--bs-primary-emphasis'),
-    }]
-  },
-  options: {
-    scales: {
-      x: {
-        type: 'linear',
-        min: 0,
-        ticks: {
-          stepSize: 1
-        },
-        title: {
-          display: true,
-          text: 'Time (seconds)',
-          color: bootstrapStyle.getPropertyValue('--bs-secondary')
-        },
-        grid: {
-          color: '#2b3035'
-        }
-      },
-      y: {
-        title: {
-          display: true,
-          text: 'Water Level (meters)',
-          color: bootstrapStyle.getPropertyValue('--bs-secondary')
-        },
-        grid: {
-          color: '#2b3035'
-        }
-      }
+    type: 'line',
+    data: {
+        datasets: [{
+            label: 'Water level',
+            borderWidth: 1,
+            tension: 0.3,
+            backgroundColor: bootstrapStyle.getPropertyValue('--bs-primary-emphasis'),
+            borderColor: bootstrapStyle.getPropertyValue('--bs-primary-emphasis'),
+        }]
     },
-    responsive: true,
-    maintainAspectRatio: false
-  }
+    options: {
+        scales: {
+            x: {
+                type: 'linear',
+                min: 0,
+                ticks: {
+                    stepSize: 1
+                },
+                title: {
+                    display: true,
+                    text: 'Time (seconds)',
+                    color: bootstrapStyle.getPropertyValue('--bs-secondary')
+                },
+                grid: {
+                    color: '#2b3035'
+                }
+            },
+            y: {
+                title: {
+                    display: true,
+                    text: 'Water Level (meters)',
+                    color: bootstrapStyle.getPropertyValue('--bs-secondary')
+                },
+                grid: {
+                    color: '#2b3035'
+                }
+            }
+        },
+        responsive: true,
+        maintainAspectRatio: false
+    }
 });
 
 const maxChartWindowSize = 16;
@@ -49,14 +49,14 @@ let currentXAxisMin = 1;
 let currentLabel = 0;
 
 function addChartData(newData) {
-  if (chart.data.labels.length >= maxChartWindowSize) {
-    chart.data.labels.shift();
-    chart.data.datasets[0].data.shift();
-    chart.options.scales.x.min = currentXAxisMin;
-    currentXAxisMin++;
-  }
-  chart.data.labels.push(currentLabel);
-  chart.data.datasets[0].data.push(newData)
-  chart.update();
-  currentLabel++;
+    if (chart.data.labels.length >= maxChartWindowSize) {
+        chart.data.labels.shift();
+        chart.data.datasets[0].data.shift();
+        chart.options.scales.x.min = currentXAxisMin;
+        currentXAxisMin++;
+    }
+    chart.data.labels.push(currentLabel);
+    chart.data.datasets[0].data.push(newData)
+    chart.update();
+    currentLabel++;
 }
