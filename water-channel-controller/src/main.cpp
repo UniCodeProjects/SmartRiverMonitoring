9150d8b1177bdfec50994c99be9e43219fb14cfc
Raffaele Francesco Marrazzo <raffaele.marrazzo@studio.unibo.it>
2024-05-07 15:55:18 +0200

    Added serial timeout in order to avoid overrun exception in AutomaticTask

--- water-channel-controller/src/main.cpp
+++ water-channel-controller/src/main.cpp
@@ -12,6 +12,7 @@
 #include "../include/task/ManualTask.h"
 
 #define SERIAL_BAUD_RATE 9600
+#define SERIAL_TIMEOUT_MILLIS 50
 
 LiquidCrystal_I2C* monitor = new LiquidCrystal_I2C(0x27, 20, 4);
 Button* const button = new ButtonImpl(2);
@@ -23,6 +24,7 @@ Scheduler scheduler;
 
 void setup() {
   Serial.begin(SERIAL_BAUD_RATE);
+  Serial.setTimeout(SERIAL_TIMEOUT_MILLIS); // Serial timeout in order to avoid overrun exception in AutomaticTask
   /*
    * This instruction is needed because Arduino resets when the server opens a serial connection with it.
    * In this way, data will be lost because the device is not ready to receive them.
