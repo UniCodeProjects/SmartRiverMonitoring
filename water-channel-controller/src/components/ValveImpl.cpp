39a305ea0061103ce14d076ae9d251fa84bbfcaf
Raffaele Francesco Marrazzo <raffaele.marrazzo@studio.unibo.it>
2024-05-07 18:04:39 +0200

    Added method getLevel() in class Valve

--- water-channel-controller/src/components/ValveImpl.cpp
+++ water-channel-controller/src/components/ValveImpl.cpp
@@ -1,19 +1,18 @@
-#include "../include/components/ValveKnob.h"
 #include "../include/components/ValveImpl.h"
 
 #include <Arduino.h>
+
+#include "../include/components/ValveKnob.h"
 
 ValveImpl::ValveImpl(ServoMotor* const servoMotor) {
     this->servoMotor = servoMotor;
 }
 
 void ValveImpl::setLevel(const int level) {
-    if (level < 0) {
-        servoMotor->setAngle(SERVO_MIN_ANGLE);
-        return;
-    } else if (level > VALVE_KNOB_LEVELS) {
-        servoMotor->setAngle(SERVO_MAX_ANGLE);
-        return;
-    }
-    servoMotor->setAngle(map(level, 0, VALVE_KNOB_LEVELS, SERVO_MIN_ANGLE, SERVO_MAX_ANGLE));
+    this->level = level < 0 ? SERVO_MIN_ANGLE : (level > VALVE_KNOB_LEVELS ? SERVO_MAX_ANGLE : level);
+    servoMotor->setAngle(map(this->level, 0, VALVE_KNOB_LEVELS, SERVO_MIN_ANGLE, SERVO_MAX_ANGLE));
+}
+
+int ValveImpl::getLevel() {
+    return this->level;
 }
